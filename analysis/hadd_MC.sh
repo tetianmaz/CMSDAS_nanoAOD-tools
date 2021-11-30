785827a1ff597012c9c99a3a6245bb80107b113b
= <Frank.Jensen@cern.ch>
2021-11-30 15:51:53 -0600

    empty hadd templates

--- analysis/hadd_MC.sh
+++ analysis/hadd_MC.sh
@@ -8,26 +8,21 @@ cd /uscmst1b_scratch/lpc1/3DayLifetime/fojensen
 
 setenv OUTPUTDIR "root://cmseos.fnal.gov//store/user/cmsdas/2022/short_exercises/Tau/"
 
-python haddnano.py DYJetsToLL_M50.0.root `xrdfsls -u /store/user/fojensen/cmsdas_28112021/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_cmsdas_DYJetsToLL_M50/211129_053308/0000/ | grep \.root`
-python haddnano.py DYJetsToLL_M50.1.root `xrdfsls -u /store/user/fojensen/cmsdas_28112021/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_cmsdas_DYJetsToLL_M50_ext/211129_053354/0000/ | grep \.root`
+python haddnano.py DYJetsToLL_M50.0.root `xrdfsls -u | grep \.root`
+python haddnano.py DYJetsToLL_M50.1.root `xrdfsls -u | grep \.root`
 python haddnano.py DYJetsToLL_M50.root DYJetsToLL_M50.0.root DYJetsToLL_M50.1.root
 xrdcp -f DYJetsToLL_M50.root $OUTPUTDIR
 
-python haddnano.py TTToSemiLeptonic.root `xrdfsls -u /store/user/fojensen/cmsdas_28112021/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/crab_cmsdas_TTToSemiLeptonic/211129_053443/0000/ | grep \.root`
+python haddnano.py TTToSemiLeptonic.root `xrdfsls -u | grep \.root`
 xrdcp -f TTToSemiLeptonic.root $OUTPUTDIR
 
-python haddnano.py TTTo2L2Nu.root `xrdfsls -u /store/user/fojensen/cmsdas_28112021/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/crab_cmsdas_TTTo2L2Nu/211129_053529/0000/ | grep \.root`
+python haddnano.py TTTo2L2Nu.root `xrdfsls -u | grep \.root`
 xrdcp -f TTTo2L2Nu.root $OUTPUTDIR
 
-python haddnano.py WJetsToLNu.root `xrdfsls -u /store/user/fojensen/cmsdas_28112021/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/crab_cmsdas_WJetsToLNu/211129_054011/0000/ | grep \.root`
+python haddnano.py WJetsToLNu.root `xrdfsls -u | grep \.root`
 xrdcp -f WJetsToLNu.root $OUTPUTDIR
 
-python haddnano.py QCD_Mu15.root `xrdfsls -u /store/user/fojensen/cmsdas_28112021/QCD_Pt-20_MuEnrichedPt15_TuneCP5_13TeV-pythia8/crab_cmsdas_QCD_Pt-20_MuEnrichedPt15/211129_054108/0000/ | grep \.root`
+python haddnano.py QCD_Mu15.root `xrdfsls -u | grep \.root`
 xrdcp -f QCD_Mu15.root $OUTPUTDIR
-
-#python haddnano.py .root `xrdfsls -u | grep \.root`
-#xrdcp -f .root $OUTPUTDIR
-
-#rm *_2018.root
 
 cd ${CURRENTDIR}
--- analysis/hadd_Muon.sh
+++ analysis/hadd_Muon.sh
@@ -8,21 +8,16 @@ cd /uscmst1b_scratch/lpc1/3DayLifetime/fojensen
 
 setenv OUTPUTDIR "root://cmseos.fnal.gov//store/user/cmsdas/2022/short_exercises/Tau/"
 
-python haddnano.py SingleMuon_2018A.root `xrdfsls -u /store/user/fojensen/cmsdas_27112021/SingleMuon/crab_cmsdas_Muon_2018A/211127_223308/0000/ | grep \.root`
+python haddnano.py SingleMuon_2018A.root `xrdfsls -u | grep \.root`
 xrdcp -f SingleMuon_2018A.root $OUTPUTDIR
 
-python haddnano.py SingleMuon_2018B.root `xrdfsls -u /store/user/fojensen/cmsdas_27112021/SingleMuon/crab_cmsdas_Muon_2018B/211127_174401/0000/ | grep \.root`
+python haddnano.py SingleMuon_2018B.root `xrdfsls -u | grep \.root` 
 xrdcp -f SingleMuon_2018B.root $OUTPUTDIR
 
-python haddnano.py SingleMuon_2018C.root `xrdfsls -u /store/user/fojensen/cmsdas_27112021/SingleMuon/crab_cmsdas_Muon_2018C/211127_174505/0000/ | grep \.root`
+python haddnano.py SingleMuon_2018C.root `xrdfsls -u | grep \.root`
 xrdcp -f SingleMuon_2018C.root $OUTPUTDIR
 
-python haddnano.py SingleMuon_2018D.root `xrdfsls -u /store/user/fojensen/cmsdas_27112021/SingleMuon/crab_cmsdas_Muon_2018D/211127_174550/0000/ | grep \.root`
+python haddnano.py SingleMuon_2018D.root `xrdfsls -u | grep \.root`
 xrdcp -f SingleMuon_2018D.root $OUTPUTDIR
-
-#python haddnano.py SingleMuon_2018.root SingleMuon_2018A.root SingleMuon_2018B.root SingleMuon_2018C.root SingleMuon_2018D.root
-#xrdcp -f SingleMuon_2018.root $OUTPUTDIR
-
-#rm SingleMuon*_2018.root
 
 cd ${CURRENTDIR}
